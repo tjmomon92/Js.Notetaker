b34afe61525be46fbd307428b116ed8f0b502f89
TJ Momon <tjmomon92@gmail.com>
2023-04-28 12:31:30 -0500

    set up GET requests for both HTML pages

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,15 @@ const fs = require('fs');
 const app = express();
 const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+// Get requests for index and notes HTMLs
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // port setup
 app.listen(PORT, () =>
